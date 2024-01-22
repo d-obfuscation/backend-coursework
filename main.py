38ebd99515dec892e62b86ca9914ea5db352cb87
YouCantOSINTme <you-cant@OSINT.me>
2024-01-22 14:20:24 +0000

    i finally got shit done for fucks sake

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    context ={"request": request}
+    return templates.TemplateResponse("./home/index.html", context)
+
+@app.get("/books", response_class=HTMLResponse)
+def index(request: Request):
+    context ={"request": request}
+    return templates.TemplateResponse("./books/books.html", context)
+
+@app.get("/authors", response_class=HTMLResponse)
+def index(request: Request):
+    context ={"request": request}
+    return templates.TemplateResponse("./authors/authors.html", context)
